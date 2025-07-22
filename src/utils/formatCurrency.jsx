88e3e484579c3bf48a74a3ae7dd7b8e1d26f60f8
Garima Khandekar <garima.k.03@gmail.com>
2025-07-22 12:44:37 -0700

    cart appears when item is added, disappears otherwise

--- src/utils/formatCurrency.jsx
+++ src/utils/formatCurrency.jsx
@@ -1,4 +1,5 @@
 const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+    // From MDN: The runtime's (browser's or something else) default locale is used when 'undefined' is passed
     currency: 'USD',
     style: 'currency'
 })
