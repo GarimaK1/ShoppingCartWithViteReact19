b026c5dc55e817401a706f2c357a3571d880fe4b
Garima Khandekar <garima.k.03@gmail.com>
2025-07-22 17:52:47 -0700

    minor enhancements

--- README.md
+++ README.md
@@ -3,7 +3,3 @@ Demo Shopping Cart functionality using React 19
 
 This relies on the fact that all products that can be added to the cart are present in a single array in the data/index.js file.
 We don't add or remove products from cart, just update properties of each product like inCart and quantity.
-
-Enhancement:
-Cart icon should always be visible. Click on cart & cross icons should control open/close cart.
-Responsiveness
--- src/components/MyShoppingCart.jsx
+++ src/components/MyShoppingCart.jsx
@@ -35,59 +35,64 @@ const MyShoppingCart = () => {
 
     return (
         <>
-            {
-                cartItems.length !== 0 && (
-                    <div className={`fixed top-0 z-30 w-[300px] h-screen bg-zinc-100 
-                            border-l-4 border-red-200 rounded-tl-lg rounded-bl-lg
-                            ${isOpen ? 'right-0' : '-right-[300px]'}`}
-                    >
-                        <div className='absolute top-0 left-0 z-10 w-full h-16 bg-white
-                                grid place-items-center border border-zinc-200 rounded-lg'
-                        >
-                            <h1 className='text-xl text-stone-600'>My Cart</h1>
-                            <button className='absolute right-3 z-20 w-9 h-9 bg-yellow-400 
-                                    grid place-items-center border-2 border-zinc-200 rounded-full
-                                    hover:bg-yellow-500 transition-colors cursor-pointer'
-                                onClick={() => setIsOpen(false)}
-                            >
-                                <X className='text-white' />
-                            </button>
-                        </div>
-                        <button className='absolute -left-14 top-3 z-20 w-9 h-9 bg-yellow-400
-                                    grid place-items-center border-2 border-zinc-200 rounded-full 
-                                    hover:bg-yellow-500 transition-colors cursor-pointer'
-                            onClick={() => setIsOpen(true)}
-                        >
-                            <ShoppingCart className='text-xs text-white' />
-                            <span className='absolute -bottom-4 -left-2 w-6 h-6 bg-[#4d4637]
+            <div className='fixed top-0 right-0 z-30 '>
+                <button className={`absolute ${isOpen ? 'right-[320px]' : '-left-14'} top-3 z-20 w-9 h-9 bg-yellow-400
+                                    grid place-items-center border border-zinc-200 rounded-full 
+                                    hover:bg-yellow-500 transition-colors cursor-pointer`}
+                    onClick={() => setIsOpen(true)}
+                >
+                    <ShoppingCart className={`text-xs ${totalQuantity > 0 ? 'text-white' : 'text-[#4d4637]'}`} />
+                    <span className={`${totalQuantity <= 0 ? 'hidden' : `absolute -bottom-4 -left-2 w-6 h-6 bg-[#4d4637]
                                     grid place-items-center border border-zinc-200 
-                                    rounded-full text-xs text-white'
-                            >
-                                {
-                                    totalQuantity > 9 ? '9+' : totalQuantity
-                                }
-                            </span>
-                        </button>
-                        <div className='h-screen flex flex-col gap-y-3 overflow-y-scroll px-5 pb-24 pt-20'>
-                            {
-                                cartItems?.map((cartItem) => {
-                                    return <CartItem key={cartItem.id} item={cartItem} fromCart={true} />
-                                })
-                            }
-                        </div>
-                        <div className='absolute bottom-0 left-0 z-10 w-full h-20 pb-2 bg-white
+                                    rounded-full text-xs text-white`}`}
+                    >
+                        {
+                            totalQuantity > 9 ? '9+' : totalQuantity
+                        }
+                    </span>
+                </button>
+            </div>
+
+            <div className={`fixed top-0 right-0 z-30 w-[300px] h-screen bg-zinc-100 
+                            border-l-4 border-red-200 rounded-tl-lg rounded-bl-lg
+                            ${!isOpen && 'hidden'}`}
+            >
+                <div className='absolute top-0 left-0 z-10 w-full h-16 bg-white
                                 grid place-items-center border border-zinc-200 rounded-lg'
-                        >
-                            <h1 className='text-base text-stone-600'>Total: {formatCurrency(totalPrice)} </h1>
-                            <button className='rounded-md bg-teal-400 px-4 py-1 text-white text-lg
+                >
+                    <h1 className='text-xl text-stone-600'>My Cart</h1>
+                    <button className='absolute right-3 z-20 w-9 h-9 bg-yellow-400 
+                                    grid place-items-center border border-zinc-200 rounded-full
+                                    hover:bg-yellow-500 transition-colors cursor-pointer'
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <X className='text-white' />
+                    </button>
+                </div>
+
+                <div className='h-screen flex flex-col gap-y-3 overflow-y-scroll px-5 pb-24 pt-20'>
+                    {
+                        totalQuantity ? (
+                            cartItems?.map((cartItem) => {
+                                return <CartItem key={cartItem.id} item={cartItem} fromCart={true} />
+                            })
+                        ) : (
+                            <p className='text-base text-stone-600 text-center'>Your cart is empty.</p>
+                        )
+                    }
+                </div>
+                <div className='absolute bottom-0 left-0 z-10 w-full h-20 pb-2 bg-white
+                                grid place-items-center border border-zinc-200 rounded-lg'
+                >
+                    <h1 className='text-base text-stone-600'>Total: {formatCurrency(totalPrice)} </h1>
+                    <button className='rounded-md bg-teal-400 px-4 py-1 text-white text-lg
                                     hover:bg-teal-500 transition-colors cursor-pointer'
-                            >
-                                Buy Now
-                            </button>
-                        </div>
-                    </div>
-                )
-            }
+                    >
+                        Buy Now
+                    </button>
+                </div>
+            </div>
+
         </>
     )
 }
