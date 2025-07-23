806d92f7b6ae7dbc475b953c5f5405590e9f9672
Garima Khandekar <garima.k.03@gmail.com>
2025-07-22 18:11:01 -0700

    files added for deployment to github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ShoppingCartWithViteReact19/',
   plugins: [
     react(),
     tailwindcss(),
